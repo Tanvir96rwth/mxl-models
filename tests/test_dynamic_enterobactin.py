483f8700f8634285f2471ee0638d879d8eb7e1a3
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-05-15 07:35:16 +0200

    fix: tests

--- tests/test_dynamic_enterobactin.py
+++ tests/test_dynamic_enterobactin.py
@@ -6,7 +6,12 @@ from mxlmodels import get_dynamic_enterobactin
 def test_rhs() -> None:
     model = get_dynamic_enterobactin()
     expected = pd.Series(
-        {"e_coli": 8.0, "c_gluta": 3.975, "enterobactin": -2.9794871794871796}
+        {
+            "x1": 0.12093348356070453,
+            "x2": 0.2355964874705504,
+            "s1": -0.7399340495204443,
+            "p1": 0.0002614990261410044,
+        }
     )
     pd.testing.assert_series_equal(
         model.get_right_hand_side().loc[expected.index],
